546a26df399dcde5f8f90cccc7c63add2afdc7b4
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-07-18 22:52:37 +0530

    init

--- src/msg.rs
+++ src/msg.rs
@@ -2,6 +2,8 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use cosmwasm_std::Addr;
 use cw721_base::Extension;
+use cosmwasm_std::{Uint128, Binary};
+// use std::marker::PhantomData;
 
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -21,6 +23,7 @@ pub struct InstantiateMsg {
 
 pub enum ExecuteMsg {
     Receive(Cw20ReceiveMsg),
+    CustomMsg { val: String },
 }
 
 pub struct Cw20ReceiveMsg {
@@ -29,92 +32,10 @@ pub struct Cw20ReceiveMsg {
     pub msg: Binary,
 }
 
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn execute(
-    deps: DepsMut,
-    _env: Env,
-    info: MessageInfo,
-    msg: ExecuteMsg,
-) -> Result<Response, ContractError> {
-    match msg {
-        ExecuteMsg::Receive(Cw20ReceiveMsg {
-            sender,
-            amount,
-            msg,
-        }) => execute_receive(deps, info, sender, amount, msg),
-    }
-}
-
-pub fn execute_receive(
-    deps: DepsMut,
-    info: MessageInfo,
-    sender: String,
-    amount: Uint128,
-    _msg: Binary,
-) -> Result<Response, ContractError> {
-    let mut config = CONFIG.load(deps.storage)?;
-    if config.cw20_address != info.sender {
-        return Err(ContractError::UnauthorizedTokenContract {});
-    }
-
-    if config.cw721_address == None {
-        return Err(ContractError::Uninitialized {});
-    }
-
-    if config.unused_token_id >= config.max_tokens {
-        return Err(ContractError::SoldOut {});
-    }
-
-    if amount != config.unit_price {
-        return Err(ContractError::WrongPaymentAmount {});
-    }
-
-    let mint_msg = Cw721ExecuteMsg::<Extension, Empty>::Mint(MintMsg::<Extension> {
-        token_id: config.unused_token_id.to_string(),
-        owner: sender,
-        token_uri: config.token_uri.clone().into(),
-        extension: config.extension.clone(),
-    });
-
-    match config.cw721_address.clone() {
-        Some(cw721) => {
-            let callback =
-                Cw721Contract::<Empty, Empty>(cw721, PhantomData, PhantomData).call(mint_msg)?;
-            config.unused_token_id += 1;
-            CONFIG.save(deps.storage, &config)?;
-
-            Ok(Response::new().add_message(callback))
-        }
-        None => Err(ContractError::Cw721NotLinked {}),
-    }
-}
-
-//const mint_msg = Cw721ExecuteMsg::<Extension, Empty>::Mint(MintMsg::<Extension> {
-const MINT_MSG = Cw721ExecuteMsg::<Extension, Empty>::Mint(MintMsg::<Extension> {
-
-// const MINT_MSG: TypeOfMintMsg = Cw721ExecuteMsg::<Extension, Empty>::Mint(MintMsg::<Extension> { 
-//     // ... });
-
-    token_id: config.unused_token_id.to_string(),
-    owner: sender,
-    token_uri: config.token_uri.clone().into(),
-    extension: config.extension.clone(),
-});
-
-match config.cw721_address.clone() {
-    Some(cw721) => {
-        let callback =
-            Cw721Contract::<Empty, Empty>(cw721, PhantomData, PhantomData).call(mint_msg)?;
-        config.unused_token_id += 1;
-        CONFIG.save(deps.storage, &config)?;
-
-        Ok(Response::new().add_message(callback))
-    }
-    None => Err(ContractError::Cw721NotLinked {}),
-}
 
 pub enum QueryMsg {
     GetConfig {},
+    CustomMsg { val: String },
 }
 
 pub struct ConfigResponse {
@@ -129,39 +50,26 @@ pub struct ConfigResponse {
     pub extension: Extension,
     pub unused_token_id: u32,
 }
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-    match msg {
-        QueryMsg::GetConfig {} => to_binary(&query_config(deps)?),
-    }
-}
 
-fn query_config(deps: Deps) -> StdResult<ConfigResponse> {
-    let config = CONFIG.load(deps.storage)?;
-    Ok(ConfigResponse {
-        owner: config.owner,
-        cw20_address: config.cw20_address,
-        cw721_address: config.cw721_address,
-        max_tokens: config.max_tokens,
-        unit_price: config.unit_price,
-        name: config.name,
-        symbol: config.symbol,
-        token_uri: config.token_uri,
-        extension: config.extension,
-        unused_token_id: config.unused_token_id,
-    })
-}
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum ExecuteMsg {
-    CustomMsg { val: String },
-}
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+// pub struct Config {
+//     pub unused_token_id: u64,
+//     pub token_uri: String,
+//     pub extension: Extension,
+//     pub cw721_address: Option<Addr>,
+// }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum QueryMsg {
-    CustomMsg { val: String },
-}
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+// #[serde(rename_all = "snake_case")]
+// pub enum ExecuteMsg {
+//     CustomMsg { val: String },
+// }
+
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+// #[serde(rename_all = "snake_case")]
+// pub enum QueryMsg {
+//     CustomMsg { val: String },
+// }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
